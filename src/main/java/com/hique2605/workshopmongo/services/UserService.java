febf2cc250f0a1a68a0775438ebeefa22db502e0
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-14 12:03:13 -0300

    Inserção de usuário com POST

--- src/main/java/com/hique2605/workshopmongo/services/UserService.java
+++ src/main/java/com/hique2605/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hique2605.workshopmongo.domain.User;
+import com.hique2605.workshopmongo.dto.UserDTO;
 import com.hique2605.workshopmongo.repository.UserRepository;
 import com.hique2605.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,4 +27,16 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 
+	
+	public User insert( User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+	
+	
+	
+	
 }
